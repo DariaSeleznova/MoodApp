1f47eebdd125cb3aaf90c408ece36dc84debb73e
Daria Seleznova <dariaseleznova1@gmail.com>
2026-03-28 08:22:01 +0100

    init: setup project structure with webpack and SCSS

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/js/index.js',
+
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        clean: true
+    },
+
+    mode: 'development',
+
+    devServer: {
+        static: './dist',
+        port: 3000,
+        open: true
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            }
+        ]
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './index.html'
+        })
+    ]
+};
